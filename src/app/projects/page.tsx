ce9a8f9879c9dc0fd90f1841c086908737955834
JulianSerante <julian.serante@gmail.com>
2023-08-11 12:44:56 -0300

    ...

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,9 +1,25 @@
-import { projects } from "@/data"
+'use client'
+
+import { projects as projectsData } from "@/data"
 import Layout from "../_app"
 import ProjectCard from "@/components/ProjectCard"
 import ProjectsNavBar from "@/components/ProjectsNavBar"
+import { useState } from "react"
+import { Category } from "@/type"
 
 const Projects = () => {
+
+  const [projects, setProjects] = useState(projectsData)
+  const [active, setActive] = useState('all')
+
+  const handleFilterCategory = (category:Category | 'all') => {
+    if(category === 'all'){
+      setProjects(projectsData);
+      setActive(category);
+      return;
+    }
+  }
+
   return (
     <Layout>
       <div className="px-5 py-2 overflow-y-scroll " style={{ height: "65vh" }}>
@@ -12,7 +28,7 @@ const Projects = () => {
         <div className="relative grid grid-cols-12 gap-4 my-3">
           {
             projects.map(project => (
-              <div className="col-span-12 p-2 bg-gray-200 rounded-lg sm:col-span-6 lg:col-span-4 dark:bg-dark-200">
+              <div className="col-span-12 p-2 bg-gray-200 rounded-lg sm:col-span-6 lg:col-span-4 dark:bg-dark-200" key={project.name}>
                 <ProjectCard project={project} key={project.name}/>
               </div>
             ))
