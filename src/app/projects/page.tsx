556c1221bc184b9806bc7cf2b485c900123f805b
JulianSerante <julian.serante@gmail.com>
2023-08-11 18:11:34 -0300

    projects page almost finished

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -18,12 +18,16 @@ const Projects = () => {
       setActive(category);
       return;
     }
+
+    const newArray = projectsData.filter(project => project.category.includes(category));
+    setProjects(newArray)
+    setActive(category)
   }
 
   return (
     <Layout>
       <div className="px-5 py-2 overflow-y-scroll " style={{ height: "65vh" }}>
-        <ProjectsNavBar/>
+        <ProjectsNavBar handleFilterCategory = {handleFilterCategory} active = {active}/>
 
         <div className="relative grid grid-cols-12 gap-4 my-3">
           {
--- src/components/ProjectsNavBar.tsx
+++ src/components/ProjectsNavBar.tsx
@@ -1,26 +1,34 @@
 import { Category } from "@/type";
 import { FunctionComponent } from "react";
 
-export const NavItem:FunctionComponent<{value:Category | 'all'}> = ({value}) => {
+export const NavItem:FunctionComponent<{
+    value:Category | 'all', 
+    handleFilterCategory:Function
+    active:string
+}> = ({value, handleFilterCategory, active}) => {
+
+    let className = "capitalize cursor-pointer hover:text-green"
+    if (active === value) className += " text-green";
+
     return (
-        <li className="capitalize cursor-pointer hover:text-green">
+        <li className={className} onClick={() => handleFilterCategory(value)}>
             {value}
         </li>
     )
 }
 
-const ProjectsNavBar = () => {
+const ProjectsNavBar:FunctionComponent<{handleFilterCategory:Function, active:string}> = (props) => {
     return (
         <div className="flex px-3 py-2 space-x-3 overflow-x-auto list-none">
-            <NavItem value="all"/>
-            <NavItem value="react"/>
-            <NavItem value="next"/>
-            <NavItem value="redux"/>
-            <NavItem value="redux toolkit"/>
-            <NavItem value="node"/>
-            <NavItem value="express"/>
-            <NavItem value="postgre"/>
-            <NavItem value="mongo"/>
+            <NavItem value="all" {...props}/>
+            <NavItem value="react" {...props}/>
+            <NavItem value="next" {...props}/>
+            <NavItem value="redux" {...props}/>
+            <NavItem value="redux toolkit" {...props}/>
+            <NavItem value="node" {...props}/>
+            <NavItem value="express" {...props}/>
+            <NavItem value="postgre" {...props}/>
+            <NavItem value="mongo" {...props}/>
         </div>
     )
 }
